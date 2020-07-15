e4674f605330d738a7741d9bd328177bdd7bd486
Orcthanc <samuel@knoethig.net>
2020-07-15 21:21:09 +0200

    Improved functionality

--- lex_yacc.py
+++ lex_yacc.py
@@ -5,9 +5,11 @@ from json import load
 import shutil
 import urllib.request as url
 from subprocess import run
+import os
 
 message = ""
 characters = {}
+charDefs = {}
 
 def loadFromJSON(name):
     with open("files/{}.json".format(name), 'rb') as jfile:
@@ -42,10 +44,36 @@ class RollResult:
         self.res = res
         self.roll = roll
 
+    def addStrs(self, other):
+        if not self.roll:
+            return other.roll
+        elif not other.roll:
+            return self.roll
+        else:
+            return self.roll + ", " + other.roll
+
     def __add__(self, other):
         if isinstance(other, RollResult):
-            return RollResult(self.res + other.res, self.roll + ", " + other.roll)
+            return RollResult(self.res + other.res, self.addStrs(other))
         return RollResult(self.res + other, self.roll)
+
+    def __sub__(self, other):
+        if isinstance(other, RollResult):
+            return RollResult(self.res - other.res, self.addStrs(other))
+        return RollResult(self.res - other, self.roll)
+
+    def __mul__(self, other):
+        if isinstance(other, RollResult):
+            return RollResult(self.res * other.res, self.addStrs(other))
+        return RollResult(self.res * other, self.roll)
+
+    def __truediv__(self, other):
+        if isinstance(other, RollResult):
+            return RollResult(self.res / other.res, self.addStrs(other))
+        return RollResult(self.res / other, self.roll)
+
+    def __neg__(self):
+        return RollResult(-self.res, self.roll)
 
     def __gt__(self, other):
         if isinstance(other, RollResult):
@@ -67,18 +95,132 @@ class RollResult:
             return self.res <= other.res
         return self.res <= other
 
-    def __mul__(self, other):
-        if isinstance(other, RollResult):
-            return RollResult(self.res * other.res, self.roll + other.roll)
-        return RollResult(self.res * other, self.roll)
-
-    def __div__(self, other):
-        if isinstance(other, RollResult):
-            return RollResult(self.res / other.res, self.roll + other.roll)
-        return RollResult(self.res / other, self.roll)
-
     def __str__(self):
-        return "{}: {{{}}}".format(self.res, self.roll)
+        return "{{{}}}: **{}**".format(self.roll, self.res)
+
+class Math_Element:
+    def execute(self):
+        raise Exception
+
+class Math_Element_Comp:
+    def __init__(self):
+        self.exprs = []
+
+    def add(self, other):
+        self.exprs.append(other)
+
+    def execute(self):
+        return "\n".join(map(lambda x: "{}".format(x.execute()), self.exprs))
+
+class Constant(Math_Element):
+    def __init__(self, value):
+        self.value = value
+
+    def execute(self):
+        return self.value
+
+class Roll(Math_Element):
+    def __init__(self, amount, size):
+        self.amount = amount
+        self.size = size
+
+    def execute(self):
+        tmpamt = self.amount.execute()
+        tmpsz = self.size.execute()
+        amount = int(tmpamt.res)
+        size = int(tmpsz.res)
+
+        if size == 0 or amount == 0:
+            return RollResult(0, tmpamt.roll + tmpsz.roll)
+
+        negative = amount < 0
+        amount = abs(amount)
+
+        print("{} {}".format(amount, size))
+        if amount > 1000000:
+            raise SyntaxError("Will not roll more than 1,000,000 dice in one roll")
+        res = []
+        for i in range(0, amount):
+            res.append(randrange(1, size + 1, 1))
+        if(amount < 200):
+            res.sort()
+            rolls = "{" + ", ".join(map(str, res)) + "}"
+        else:
+            rolls = "{ Omitted because more than 200 die were rolled }"
+        return RollResult(-sum(res) if negative else sum(res), tmpamt.roll + tmpsz.roll + rolls)
+
+class ComplicatedRoll(Math_Element):
+    def __init__(self, amount, size, keep, high):
+        self.amount = amount
+        self.size = size
+        self.keep = keep
+        self.high = high
+
+    def execute(self):
+        tmpamt = self.amount.execute()
+        tmpsz = self.size.execute()
+        tmpkp = self.keep.execute()
+        amount = int(tmpamt.res)
+        size = int(tmpsz.res)
+        keep = int(tmpkp.res)
+
+        if size == 0 or amount == 0 or keep == 0:
+            return RollResult(0, tmpamt.roll + tmpsz.roll + tmpkp.roll)
+
+        negative = amount < 0
+        amount = abs(amount)
+
+        print("{} {}".format(amount, size))
+        if amount > 1000000:
+            raise SyntaxError("Will not roll more than 1,000,000 dice in one roll")
+        res = []
+        for i in range(0, amount):
+            res.append(randrange(1, size + 1, 1))
+        res.sort()
+        if self.high:
+            if(amount < 200):
+                rolls = "{~~" + ", ".join(map(str, res[:-keep]))+ "~~, " + ", ".join(map(str, res[-keep:])) + "}"
+            else:
+                rolls = "{ Omitted because more than 200 die were rolled }"
+            return RollResult(-sum(res[-keep:]) if negative else sum(res[-keep:]), tmpamt.roll + tmpsz.roll + tmpkp.roll + rolls)
+
+        else:
+            if(amount < 200):
+                rolls = "{" + ", ".join(map(str, res[:keep]))+ ", ~~" + ", ".join(map(str, res[keep:])) + "~~}"
+            else:
+                rolls = "{ Omitted because more than 200 die were rolled }"
+            return RollResult(-sum(res[:keep]) if negative else sum(res[:keep]), tmpamt.roll + tmpsz.roll + tmpkp.roll + rolls)
+
+class Binop(Math_Element):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class Add(Binop):
+    def execute(self):
+        return self.left.execute() + self.right.execute()
+
+class Sub(Binop):
+    def execute(self):
+        return self.left.execute() - self.right.execute()
+
+class Mul(Binop):
+    def execute(self):
+        return self.left.execute() * self.right.execute()
+
+class Div(Binop):
+    def execute(self):
+        return self.left.execute() / self.right.execute()
+
+class UnMinus(Math_Element):
+    def __init__(self, value):
+        self.value = value
+
+    def execute(self):
+        return -self.value.execute()
+
+
+
 
 reserved = (
     'READ', 'REREAD', 'HELP', 'LOADCON', 'ROLL', 'DMINIT',
@@ -89,7 +231,7 @@ tokens = reserved + (
     'INTL', 'FLOATL', 'COMMAND',
 
     # Operator
-    'PLUS', 'MINUS', 'TIMES', 'DIVIDE',
+    'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'EQUALS',
 
     'COMMA', 'LBRACK', 'RBRACK', 'SEMICOLON',
 
@@ -111,6 +253,7 @@ t_COMMA = r','
 t_LBRACK = r'\('
 t_RBRACK = r'\)'
 t_SEMICOLON = r';'
+t_EQUALS = r'='
 
 t_DIE = r'd'
 t_KEEPH = r'h'
@@ -132,13 +275,14 @@ t_INTL = r'\d+'
 
 def t_error(t):
     print("Illegal character %s" % repr(t.value[0]))
-    t.lexer.skip(1)
+    raise SyntaxError("Illegal character {}".format(t.value[0]))
 
 lexer = lex.lex()
 
 import ply.yacc as yacc
 
 precedence = (
+    ('right', 'EQUALS'),
     ('left', 'PLUS', 'MINUS'),
     ('left', 'TIMES', 'DIVIDE'),
     ('nonassoc', 'DIE', 'KEEPL', 'KEEPH'),
@@ -154,6 +298,35 @@ def p_line2(p):
             | expression SEMICOLON'''
     p[0] = "{}".format(p[1])
 
+def p_line3(p):
+    'line : COMMAND EQUALS m_expression_list'
+    try:
+        if p[1] in attributes:
+            p[0] = "Error: Could not overwrite predefined command {}".format(p[1])
+            return
+    except NameError:
+        pass
+    name = "{}".format(message.author)
+    if not name in charDefs:
+        charDefs[name] = {}
+    retmsg = ""
+    if p[1] in charDefs[name]:
+        retmsg = "Warning: redecleration of identifier {}".format(p[1])
+    else:
+        retmsg = "Declared identifier {} for {}".format(p[1], message.author)
+    charDefs[name][p[1]] = p[3]
+    p[0] = retmsg
+
+def p_m_expression_list(p):
+    'm_expression_list : m_expression SEMICOLON m_expression_list'
+    p[0] = p[3]
+    p[0].add(p[1])
+
+def p_m_expression_list2(p):
+    '''m_expression_list : m_expression
+                         | m_expression SEMICOLON'''
+    p[0] = Math_Element_Comp()
+    p[0].add(p[1])
 
 def p_read_expression(p):
     'expression : READ'
@@ -172,6 +345,8 @@ def p_read_expression(p):
                 shutil.copyfileobj(pdf, pfile)
 
         run(["./PDFtoJSON", "files/{}.pdf".format(message.author), "files/{}.json".format(message.author)])
+
+        os.remove("files/{}.pdf".format(message.author))
 
         p[0] = loadFromJSON("{}".format(message.author))
     else:
@@ -210,7 +385,7 @@ def p_loadcon_expression(p):
 
 def p_roll_expression(p):
     'expression : ROLL m_expression'
-    p[0] = p[2]
+    p[0] = p[2].execute()
 
 def p_dminit_expression1(p):
     'expression : DMINIT'
@@ -218,7 +393,7 @@ def p_dminit_expression1(p):
     results.sort(key = lambda x: x[1], reverse = True)
     p[0] = "```\n"
     for x in results:
-        p[0] += "{}: {}\n".format(x[1], x[0])
+        p[0] += "{:>3}: {}\n".format(x[1], x[0])
     p[0] += "```"
 
 def p_dminit_expression2(p):
@@ -243,24 +418,31 @@ def p_arg_list2(p):
     p[0].append(p[3])
 
 def p_command_expression1(p):
-    'expression : COMMAND'
-    if not p[1] in attributes:
-        raise SyntaxError("Unknown attribute {}".format(p[1]))
-    if not "{}".format(message.author) in characters:
-        raise SyntaxError("Could not find char of {}".format(message.author))
-    p[0] = randrange(1, 21, 1) + int(characters["{}".format(message.author)].get(attributes[p[1]], "-20"))
+    'm_expression : COMMAND'
+    name = "{}".format(message.author)
 
-def p_command_expression2(p):
-    'expression : COMMAND m_expression'
-    if not p[1] in attributes:
-        raise SyntaxError("Unknown attribute {}".format(p[1]))
+    try:
+        if not p[1] in attributes:
+            if name in charDefs:
+                if p[1] in charDefs[name]:
+                    p[0] = charDefs[name][p[1]]
+                    return
+            raise SyntaxError("Unknown identifier {}".format(p[1]))
+    except NameError:
+        if name in charDefs:
+            if p[1] in charDefs[name]:
+                p[0] = charDefs[name][p[1]]
+                return
+        raise SyntaxError("Unknown identifier {}".format(p[1]))
+
     if not "{}".format(message.author) in characters:
         raise SyntaxError("Could not find char of {}".format(message.author))
-    p[0] = randrange(1, 21, 1) + int(characters["{}".format(message.author)].get(attributes[p[1]], "-20")) + p[2]
+    #p[0] = randrange(1, 21, 1) + int(characters["{}".format(message.author)].get(attributes[p[1]], "-20"))
+    p[0] = Add(Roll(Constant(RollResult(1, "")), Constant(RollResult(20, ""))), Constant(RollResult(int(characters["{}".format(message.author)].get(attributes[p[1]], "-20")), "")))
 
 def p_m_expression_expression(p):
     'expression : m_expression'
-    p[0] = p[1]
+    p[0] = p[1].execute()
 
 def p_m_expression1(p):
     '''m_expression : m_expression PLUS m_expression
@@ -268,13 +450,14 @@ def p_m_expression1(p):
                   | m_expression TIMES m_expression
                   | m_expression DIVIDE m_expression'''
     if p[2] == '+':
-        p[0] = p[1] + p[3]
+        p[0] = Add(p[1], p[3])
     elif p[2] == '-':
-        p[0] = p[1] - p[3]
+        p[0] = Sub(p[1], p[3])
     elif p[2] == '*':
-        p[0] = p[1] * p[3]
+        p[0] = Mul(p[1], p[3])
     elif p[2] == '/':
-        p[0] = p[1] / p[3]
+        p[0] = Div(p[1], p[3])
+
 
 def p_m_expression2(p):
     'number : LBRACK m_expression RBRACK'
@@ -286,7 +469,7 @@ def p_m_expression3(p):
 
 def p_m_expression4(p):
     'm_expression : MINUS m_expression %prec UMINUS'
-    p[0] = -p[2]
+    p[0] = UnMinus(p[2])
 
 def p_m_expression5(p):
     'm_expression : PLUS m_expression %prec UPLUS'
@@ -298,49 +481,21 @@ def p_m_expression6(p):
 
 def p_m_dieroll1(p):
     'dieroll : number DIE number'
-    if p[1] > 1000000:
-        raise SyntaxError("Will not roll more than 1,000,000 dice in one roll")
-    res = []
-    for i in range(0, p[1]):
-        res.append(randrange(1, p[3] + 1, 1))
-    if(p[1] < 200):
-        res.sort()
-        rolls = "{" + ", ".join(map(str, res)) + "}"
-    else:
-        rolls = "{ Omitted because more than 200 die were rolled }"
-    p[0] = RollResult(sum(res), rolls)
+    p[0] = Roll(p[1], p[3])
 
 def p_m_dieroll2(p):
     '''dieroll : number DIE number KEEPH number
                | number DIE number KEEPL number'''
-    if p[1] > 1000000:
-        raise SyntaxError("Will not roll more than 1,000,000 dice in one roll")
-    res = []
-    for i in range(0, p[1]):
-        res.append(randrange(1, p[3] + 1, 1))
-    res.sort()
-    if p[4] == 'h':
-        if(p[1] < 200):
-            rolls = "{~~" + ", ".join(map(str, res[:-p[5]]))+ "~~, " + ", ".join(map(str, res[-p[5]:])) + "}"
-        else:
-            rolls = "{ Omitted because more than 200 die were rolled }"
-        p[0] = RollResult(sum(res[-p[5]:]), rolls)
-    elif p[4] == 'l':
-        if(p[1] < 200):
-            rolls = "{" + ", ".join(map(str, res[:p[5]]))+ ", ~~" + ", ".join(map(str, res[p[5]:])) + "~~}"
-        else:
-            rolls = "{ Omitted because more than 200 die were rolled }"
-        p[0] = RollResult(sum(res[:p[5]]), rolls)
-
+    p[0] = ComplicatedRoll(p[1], p[3], p[5], p[4] == 'h')
 
 
 def p_m_number1(p):
     'number : FLOATL'
-    p[0] = float(p[1])
+    p[0] = Constant(RollResult(float(p[1]), ""))
 
 def p_m_number2(p):
     'number : INTL'
-    p[0] = int(p[1])
+    p[0] = Constant(RollResult(int(p[1]), ""))
 
 def p_error(p):
     print("Syntax error {}".format(p))
